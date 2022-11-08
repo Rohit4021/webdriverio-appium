fc421706299d3451a316c9ea70a5505dd99ec3e2
Rohit4021 <87359774+Rohit4021@users.noreply.github.com>
2022-11-08 19:46:55 +0530

    Update shell.js

--- shell.js
+++ shell.js
@@ -1,3 +1,10 @@
 const execSh = require('exec-sh')
+const express = require('express')
+const app = express()
 
-execSh("npx wdio")
+app.get('/', (req, res) => {
+    res.send("Hi")
+    execSh("npx wdio")
+})
+
+app.listen(process.env.PORT)
